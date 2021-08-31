240fe8cde5728183f50f8902cef3b03171edbd12
ynaraoliveira <ynaraoliveira.s@gmail.com>
2021-08-31 19:45:04 -0300

    modify app startup runner

--- src/main/java/com/matera/bootcamp/digitalbank/AppStartupRunner.java
+++ src/main/java/com/matera/bootcamp/digitalbank/AppStartupRunner.java
@@ -2,12 +2,21 @@ package com.matera.bootcamp.digitalbank;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AppStartupRunner implements ApplicationRunner {
+	
+	Printer printer;
+	
+	public AppStartupRunner(Printer printer) {
+		this.printer = printer;
+	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("AppStartupRunner");
+		
+		printer.print();
 		
 	}
 
---
+++ src/main/java/com/matera/bootcamp/digitalbank/Printer.java
@@ -0,0 +1,12 @@
+package com.matera.bootcamp.digitalbank;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Printer {
+	
+	public void print() {
+		System.out.println("Print");
+	}
+
+}
